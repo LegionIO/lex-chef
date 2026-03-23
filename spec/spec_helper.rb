b2e6ed940da9352444d99aef4111a97e644c04e4
Esity <matthewdiverson@gmail.com>
2026-03-22 23:09:57 -0500

    add sub-gem helper dependencies and update spec_helper (v0.2.1)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,25 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
+require 'legion/logging'
+require 'legion/settings'
+require 'legion/cache/helper'
+require 'legion/crypt/helper'
+require 'legion/data/helper'
+require 'legion/json/helper'
+require 'legion/transport/helper'
 
-unless defined?(Legion::Extensions::Helpers::Lex)
-  module Legion
-    module Extensions
-      module Helpers
-        module Lex
-          def self.included(base)
-            base.extend base if base.instance_of?(Module)
-          end
-        end
+module Legion
+  module Extensions
+    module Helpers
+      module Lex
+        include Legion::Logging::Helper
+        include Legion::Settings::Helper
+        include Legion::Cache::Helper
+        include Legion::Crypt::Helper
+        include Legion::Data::Helper
+        include Legion::JSON::Helper
+        include Legion::Transport::Helper
       end
     end
   end
